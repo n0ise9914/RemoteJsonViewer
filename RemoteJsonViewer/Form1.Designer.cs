00119d8bd9cb7b746e7d1b865faae1362b6d8a39
noise <n0ise9914@live.com>
2017-08-13 04:16:24 +0430

    init

---
+++ RemoteJsonViewer/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace HttpJsonViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.JsonTreeView = new System.Windows.Forms.TreeView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.view = new System.Windows.Forms.TabPage();
+            this.raw = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menu.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.view.SuspendLayout();
+            this.raw.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // JsonTreeView
+            // 
+            this.JsonTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.JsonTreeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.JsonTreeView.Location = new System.Drawing.Point(0, 0);
+            this.JsonTreeView.Name = "JsonTreeView";
+            this.JsonTreeView.Size = new System.Drawing.Size(504, 363);
+            this.JsonTreeView.TabIndex = 0;
+            this.JsonTreeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.JsonTreeView_NodeMouseClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(127, 368);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem});
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(103, 26);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.view);
+            this.tabControl1.Controls.Add(this.raw);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(145, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(512, 394);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // view
+            // 
+            this.view.Controls.Add(this.JsonTreeView);
+            this.view.Location = new System.Drawing.Point(4, 27);
+            this.view.Name = "view";
+            this.view.Padding = new System.Windows.Forms.Padding(3);
+            this.view.Size = new System.Drawing.Size(504, 363);
+            this.view.TabIndex = 0;
+            this.view.Text = "view";
+            this.view.UseVisualStyleBackColor = true;
+            // 
+            // raw
+            // 
+            this.raw.Controls.Add(this.richTextBox1);
+            this.raw.Location = new System.Drawing.Point(4, 27);
+            this.raw.Name = "raw";
+            this.raw.Padding = new System.Windows.Forms.Padding(3);
+            this.raw.Size = new System.Drawing.Size(504, 363);
+            this.raw.TabIndex = 1;
+            this.raw.Text = "raw";
+            this.raw.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(504, 368);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 414);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RemoteJsonViewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menu.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.view.ResumeLayout(false);
+            this.raw.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView JsonTreeView;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ContextMenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage view;
+        private System.Windows.Forms.TabPage raw;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ RemoteJsonViewer/Form1.cs
@@ -0,0 +1,87 @@
+﻿
+using JsonTreeView;
+using Newtonsoft.Json;
+using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HttpJsonViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var port = 9128;
+            this.Text = Text + " 127.0.0.1:" + port;
+            var server = new SimpleTcpServer().Start(port);
+            server.Delimiter = 0x0A;
+            server.DelimiterDataReceived += (sndr, msg) => {
+
+                if (InvokeRequired)
+                {
+                    this.Invoke(new MethodInvoker(delegate {
+                        listBox1.Items.Add(msg.MessageString);
+                        listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                    }));
+                    return;
+                }   
+            };
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+                dynamic parsedJson = JsonConvert.DeserializeObject(listBox1.SelectedItem.ToString().Trim());     
+                richTextBox1.Text = JsonConvert.SerializeObject(parsedJson, Formatting.Indented) ;
+
+                JsonTreeView.Nodes.Clear();
+                JsonTreeView.LoadJsonToTreeView(listBox1.SelectedItem.ToString().Trim());
+              //  JsonTreeView.ExpandAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid Json: " + ex.ToString());
+            }
+        }
+
+        private void JsonTreeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            JsonTreeView.SelectedNode = e.Node;
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                menu.Show(Cursor.Position);
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string json = JsonTreeView.SelectedNode.Text;
+            Clipboard.SetText(json.Substring(json.IndexOf(": ")+2));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            JsonTreeView.Nodes.Clear();
+            listBox1.Items.Clear();
+        }
+    }
+}
