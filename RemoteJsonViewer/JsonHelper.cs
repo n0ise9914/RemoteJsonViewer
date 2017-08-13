e2b63eb2cd41ba71ce3e1c40524bd6d00c739d6b
noise <n0ise9914@live.com>
2017-08-14 01:16:19 +0430

    added clinet helper sample

---
+++ RemoteJsonViewer/JsonHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace JsonTreeView
+{
+    public class JsonHelper
+    {
+        private static readonly TcpClient TcpClient = new TcpClient();
+
+        public static void Log(string json)
+        {
+            if (!TcpClient.Connected)
+            {
+                var result = TcpClient.ConnectAsync("127.0.0.1", 9128);
+                result.Wait();
+                if (TcpClient.Connected)
+                    Console.WriteLine("jsonViewer: " + (TcpClient.Connected ? "connected" : "cant connect"));
+                else
+                    return;
+            }
+
+            var networkStream = TcpClient.GetStream();
+            var clientStreamWriter = new StreamWriter(networkStream);
+            clientStreamWriter.Write(json + Environment.NewLine);
+            clientStreamWriter.Flush();
+        }
+    }
+}
